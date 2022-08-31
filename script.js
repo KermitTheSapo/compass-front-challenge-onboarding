6bf6b8a1873da1abf9e1c3b3cc4e44e2e80d4e6c
Robert <rjsf06@gmail.com>
2022-08-31 08:16:47 -0300

    :sparkles: feat: Button to change image and name working

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const button = document.querySelector(".buttonOn")
-const sectionMain = document.querySelector(".contentPageOne")
-const contentPageTwo = document.querySelector(".contentPageTwo")
-const contentPageThree = document.querySelector(".contentPageThree")
+const sectionMain = document.querySelector(".sectionMain")
 const imagem = document.querySelector(".mainImg")
+const title = document.querySelector(".title")
+const paragraph = document.querySelector(".paragraph")
 
 let contador = '0'
 
-function handleChange() {
-    if (contador === '0') {
-        button.className = "buttonOff"
-        contador = '1'
-    } else {
-        button.className = "buttonOn"
-        contador = '0'
-    }
-}
+// function handleChange() {
+//     if (contador === '0') {
+//         button.className = "buttonOff"
+//         contador = '1'
+//     } else {
+//         button.className = "buttonOn"
+//         contador = '0'
+//     }
+// }
 
 function next() {
     if (contador === '0') {
-        sectionMain.className = "contentPageTwo"
-        contentPageTwo.setAttribute('style', 'display: block')
         imagem.setAttribute('src', "imgs/page2.png")
+        title.innerHTML = "Get fast & safe delivery"
+        paragraph.innerHTML = "Get good quality products for your plants "
         contador = '1'
     } else if (contador === '1') {
-        sectionMain.className = "contentPageOne"
-        contentPageTwo.setAttribute('style', 'display: none')
-        imagem.setAttribute('src', "imgs/page1.png")
-        contador = '2'
-    } else if (contador ==='2'){
-        sectionMain.className = "contentPageThree"
-        contentPageThree.setAttribute('style', 'display: block')
+        title.innerHTML = "Buy & Sell Tools"
+        paragraph.innerHTML = "Buy & sell good quality products for your beautiful plants"
         imagem.setAttribute('src', "imgs/page3.png")
-        contador = '0'
-    }
+        contador = '2'
+    } 
+    // else if (contador ==='2'){
+    //     contentPageThree.setAttribute('style', 'display: block')
+    //     imagem.setAttribute('src', "imgs/page3.png")
+    //     contador = '0'
+    // }
 }
